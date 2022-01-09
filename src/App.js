ebcf143651470d7d2725c7207621a354a19dc58f
qma <zndwlrnjr26@daum.net>
2022-01-09 16:22:21 +0900

    modified README.md

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let [글제목, 글제목변경] = useState([
+    "React 공부하기",
+    "책 보기",
+    "몽고DB 공부하기",
+  ]);
+
+  function 제목바꾸기() {
+    var newArray = [...글제목];
+    newArray[0] = "리액트 공부하기";
+    글제목변경(newArray.sort());
+  }
+
+  let [좋아요, 좋아요변경] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>개발 Blog</div>
+      </div>
+      <button onClick={제목바꾸기}>변경 & 정렬</button>
+      <div className="list">
+        <h3>
+          {글제목[0]} <span onClick={() => 좋아요변경(좋아요 + 1)}> ❤ </span>
+          {좋아요}
+        </h3>
+        <p>1월 9일 발행</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3>{글제목[1]}</h3>
+        <p>1월 9일 발행</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3>{글제목[2]}</h3>
+        <p>1월 9일 발행</p>
+        <hr />
+      </div>
     </div>
   );
 }
